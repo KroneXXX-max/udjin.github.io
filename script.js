3d7c2025fb9520077ec2110f00ee915559819599
KroneXXX-max <alexkorn070408@gmail.com>
2025-12-04 14:39:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// ВЫПАДАЮЩЕЕ МЕНЮ СПРАВА
+const menuBtn = document.getElementById("menu-btn");
+const nav = document.getElementById("nav");
+
+menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("show");
+});
+
 // -------------------- ЯНДЕКС КАРТА --------------------
 ymaps.ready(init);
 
